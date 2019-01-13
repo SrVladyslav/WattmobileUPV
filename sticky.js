3b822989d164d17042dcd83ca4c798be2ccceec6
SrVladyslav <srvladys@gmail.com>
2019-01-13 21:35:31 +0100

    trying things

---
+++ sticky.js
@@ -0,0 +1,10 @@
+/*$(document).ready(function(){
+    $(window).on('scroll',function(){
+        if($(window).scrollTop()){
+            $(".sub-header").addClass('active');
+        }else{
+            $(".sub-header").removeClass('active');
+        }
+    });
+});
+*/
