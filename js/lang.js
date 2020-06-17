65bb8a17f1c6dd3b0a32f97581fb5a3d7c0f20ce
SrVladyslav <vlama@inf.upv.es>
2020-06-17 15:24:54 +0200

    New remastered version

---
+++ js/lang.js
@@ -0,0 +1,176 @@
+var dictionary = {
+    // Index
+    'bar-lang':{
+        'es':'ES',
+        'en':'EN',
+        'vlc':'VLC'
+    },
+	'bar-home':{
+		'es':'Principal',
+        'en':'Home',
+        'vlc':'Principal'
+    },
+    'bar-about-us':{
+		'es':'Nosotros',
+        'en':'About Us',
+        'vlc':'Nosaltres'
+    },
+    'bar-contact':{
+		'es':'Contacto',
+        'en':'Contact',
+        'vlc':'Contacte'
+    },
+    'bar-sponsors':{
+		'es':'Patrocinio',
+        'en':'Sponsors',
+        'vlc':'Patrocini'
+    },
+    'sponsors':{
+		'es':'Patrocinadores',
+        'en':'Sponsors',
+        'vlc':'Patrocinis'
+    },
+    'gallery':{
+		'es':'Galeria',
+        'en':'Gallery',
+        'vlc':'Galeria'
+    },
+    'about-power':{
+		'es':'Potencia',
+        'en':'Power',
+        'vlc':'Potencia'
+    },
+    'about-max-speed':{
+		'es':'Velocidad Máx.',
+        'en':'Max.Speed.',
+        'vlc':'Velocitat Máx.'
+    },
+    'about-torque':{
+		'es':'Torque Motor',
+        'en':'Motor Torque',
+        'vlc':'Torque Motor'
+    },
+    'descript':{
+		'es':'Wattmobile UPV es el primer vehiculo ( 100% ) <br>\
+        eléctrico de competición en España. Con ello, <br> pretende introducir las nuevas tecnologías <br> automovilísticas al deporte.',
+        'en':'Wattmobile UPV is the first vehicle (100%) <br>\
+        electric of competition in Spain. With that, <br>\
+        pretends to introduce new automobile technologies <br>\
+        to sport.',
+        'vlc':"Wattmobile UPV és el primer vehicle (100%) <br>\
+        elèctric de competició a Espanya. Amb això, <br>\
+        pretén introduir les noves tecnologies <br>\
+        automobilístiques a l'esport."
+    },
+    'desc-team':{
+		'es':'Un proyecto fundamentado en el aprendizaje continuo y la puesta en práctica de los conceptos teóricos que adquirimos en la Universidad. Propulsado por las bulliciosas mentes de algunos estudiantes de la Universitat Politècnica de València, Wattmobile UPV toma desde sus raíces un compromiso determinante con uno de los problemas contemporáneos más trascendentales, el calentamiento global producido por la incesante emisión de gases contaminantes a la atmósfera.\
+        <br><br>\
+        Es por esto que en Wattmobile UPV apostamos por la conversión de vehículos de combustión en eléctricos, y es nuestro compromiso mostrar que a base de trabajo en equipo e implicación podemos ser capaces de dar forma a un prototipo competitivo, capaz de batirse al más alto nivel con emisiones 0.',
+        'en':'A project based on continuous learning and the implementation of the theoretical concepts that we acquire at the University. Powered by the bustling minds of some students of the Polytechnic University of Valencia, Wattmobile UPV takes from its roots a decisive commitment to one of the most transcendental contemporary problems, the global warming produced by the incessant emission of polluting gases into the atmosphere.\
+        <br><br>\
+        That is why at Wattmobile UPV we are committed to the conversion of combustion vehicles into electric vehicles, and it is our commitment to show that based on teamwork and involvement we can be able to shape a competitive prototype, capable of beating at the highest level with emissions 0.',
+        'vlc':"Un projecte fonamentat en l'aprenentatge continu i la posada en pràctica dels conceptes teòrics que vam adquirir a la Universitat. Propulsat per les bullicioses ments d'alguns estudiants de la Universitat Politècnica de València, Wattmobile UPV pren des de les seues arrels un compromís determinant amb un dels problemes contemporanis més transcendentals, l'escalfament global produït per la incessant emissió de gasos contaminants a l'atmosfera.\
+        <br><br>\
+        És per això que en Wattmobile UPV apostem per la conversió de vehicles de combustió en elèctrics, i és el nostre compromís mostrar que a força de treball en equip i implicació podem ser capaços de donar forma a un prototip competitiu, capaç de batre a el més alt nivell amb emissions 0."
+    },
+    'motor':{
+		'es':'Empleamos un motor síncrono de flujo axial de 110 KW, el cual <br>\
+        nos permite alcanzar los 5500 rpm. La gran ventaja de este motor <br>\
+        es su relación peso poténcia. Con apenas 12 kg nos proporciona <br>\
+        130 CV de potencia.',
+        'en':'We use a 110 KW axial flow synchronous motor, which <br>\
+        allows us to reach 5500 rpm. The great advantage of this engine <br>\
+        is their power to weight ratio. With just 12 kg it provides us <br>\
+        130 hp of power.',
+        'vlc':"Fem servir un motor síncron de flux axial de 110 KW, el qual\
+        ens permet assolir els 5500 rpm. El gran avantatge d'aquest motor\
+        és la seva relació pes potència. Amb prou feines 12 kg ens proporciona\
+        130 CV de potència."
+    },
+    'battery':{
+		'es':'La batería es el centro de nuestro coche.\
+        Está diseñada y montada desde cero en las \
+        instalaciones de la universidad. Con ella \
+        logramos unos extraordinarios 10 KWh. \
+        Con unas capacidades de descarga de 150 A,\
+        que junto a sus 700 V nos proporciona 100 KW \
+        de potencia que alimentan a nuestro motor.',
+        'en':'The battery is the center of our car.\
+        It is designed and mounted from scratch in the\
+        University facilities. With her\
+        We achieved an extraordinary 10 KWh.\
+        With discharge capacities of 150 A,\
+        which together with its 700 V provides us with 100 KW\
+        of power that feed our engine.',
+        'vlc':"La bateria és el centre del nostre cotxe.\
+        Està dissenyada i muntada des de zero en les\
+        instal·lacions de la universitat. amb ella\
+        aconseguim uns extraordinaris 10 KWh.\
+        Amb unes capacitats de descàrrega de 150 A,\
+        que al costat dels seus 700 V ens proporciona 100 KW\
+        de potència que alimenten el nostre motor."
+    },
+    'contact':{
+		'es':'Contacto',
+        'en':'Contact',
+        'vlc':'Contacte'
+    },
+    'footer-text':{
+		'es':'©2020 WATTMOBILE UPV. Todos los derechos reservados.',
+        'en':'©2020 WATTMOBILE UPV. All rights reserved.',
+        'vlc':'©2020 WATTMOBILE UPV. Tots els drets reservats.'
+    },
+
+    // About US
+    'team':{
+		'es':'Equipo',
+        'en':'Team',
+        'vlc':'Equip'
+    },
+    // Cards
+    'team_electronics':{
+		'es':'Miembro de electronica',
+        'en':'Electronics member',
+        'vlc':"Membre d'electronica"
+    },
+    'team_marketing':{
+		'es':'Miembro de marketing y diseñador',
+        'en':'Marketing member and designer',
+        'vlc':'Membre de marketing y disseny'
+    },
+    'jose-juan':{
+		'es':'Miembro de: <br> - Estructuras <br> - Electronica <br> - Motor',
+        'en':'Member of: <br> - Structures <br> - Electronics <br> -Motor',
+        'vlc':'Membre de: <br> - Estructures <br> - Electrónica <br> - Motor'
+    },
+    'team_school_ETSID':{
+		'es':'Escuela: ETSID',
+        'en':'School: ETSID',
+        'vlc':'Escola: ETSID'
+    },
+    'team_school_ETSINF':{
+		'es':'Escuela: ETSINF',
+        'en':'School: ETSINF',
+        'vlc':'Escola: ETSINF'
+    },
+    'team_university':{
+		'es':'Universidad Politecnica de Valencia',
+        'en':'Polytechnic University of Valencia',
+        'vlc':'Universitat Politecnica de Valencia'
+    },
+    'team_programmer':{
+		'es':'Programador y miembro de electronica',
+        'en':'Programmer and electronics member',
+        'vlc':"Programador y membre d'electronica"
+    },
+    'gallery-title':{
+		'es':'Galeria',
+        'en':'Gallery',
+        'vlc':"Galeria"
+    },
+    'photos':{
+		'es':'Fotos',
+        'en':'Photos',
+        'vlc':"Fotos"
+    },
+}
